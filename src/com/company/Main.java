476a241ca72960e01ad181aeaa2b42320dec5daa
ningpan <ningpan@shketai.com>
2020-04-29 11:16:29 +0800

    使用Java实现基于虹软人脸识别sdk的小案例，初始化提交

---
+++ src/com/company/Main.java
@@ -0,0 +1,212 @@
+package com.company;
+import com.arcsoft.face.*;
+import com.arcsoft.face.enums.DetectMode;
+import com.arcsoft.face.enums.DetectOrient;
+import com.arcsoft.face.toolkit.ImageInfo;
+import com.company.utils.*;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.arcsoft.face.toolkit.ImageFactory.getRGBData;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // 在线激活SDK，激活之后可离线使用，程序启动激活即可，无需重复调用
+        FaceRecognitionUtils.sdkActivation();
+        // 初始化引擎
+        FaceRecognitionUtils.InitializeTheEngine(DetectMode.ASF_DETECT_MODE_IMAGE, DetectOrient.ASF_OP_0_ONLY,10,32);
+
+        File file1=new File("F:\\电脑壁纸\\宋祖儿\\5c862c467a803.jpg");
+        // 图像1信息
+        ImageInfo imageInfo1=getRGBData(file1);
+
+        File file2=new File("F:\\电脑壁纸\\宋祖儿\\5c862c467a803.jpg");
+        // 图像2信息
+        ImageInfo imageInfo2=getRGBData(file2);
+
+        System.out.println("--*--------------------------------【 图像1 】--------------------------------*--"+System.lineSeparator());
+
+
+        // 创建图像1中的人脸信息对象列表
+        List<FaceInfo> faceInfoList1=new ArrayList<>();
+        // 检测图像1中人脸
+        FaceRecognitionUtils.faceDetection1(imageInfo1,faceInfoList1);
+        // 检测图像1中人脸属性
+        FaceRecognitionUtils.faceAttributeDetection1(imageInfo1,faceInfoList1);
+
+        /* 图像1中的人脸年龄 */
+        {
+            // 创建图像1中的人脸年龄列表
+            List<AgeInfo> ageInfoList1 = new ArrayList<>();
+            // 检测图像1中的人脸年龄列表
+            FaceRecognitionUtils.getAgeInfo(ageInfoList1);
+            // 将图像1中的年龄列表打印到控制台
+            for (int i = 0; i < ageInfoList1.size(); i++) {
+                System.out.println("图像1中的第" + (i + 1) + "张人脸的性别为：" + ageInfoList1.get(i).getAge()+System.lineSeparator());
+            }
+        }
+
+        /* 图像1中的人脸性别 */
+        {
+            // 创建图像1中的人脸性别列表
+            List<GenderInfo> genderInfoList1 = new ArrayList<>();
+            // 检测图像1中的人脸性别列表
+            FaceRecognitionUtils.getGender(genderInfoList1);
+            // 将图像1中的性别列表打印到控制台
+            for (int i = 0; i < genderInfoList1.size(); i++) {
+                String sex = genderInfoList1.get(i).getGender() == 0 ? "男" : "女";
+                System.out.println("图像1中的第" + (i + 1) + "张人脸的性别为：" + sex+System.lineSeparator());
+            }
+        }
+
+        /* 图像1中的人脸三维角度 */
+        {
+            // 创建图像1中的人脸三维角度信息列表
+            List<Face3DAngle> face3DAngleList1 = new ArrayList<>();
+            // 获取图像1中的人脸三维角度信息列表
+            FaceRecognitionUtils.getFace3DAngle(face3DAngleList1);
+            // 将图像1中的人脸三维角度信息列表打印到控制台
+            for (int i = 0; i < face3DAngleList1.size(); i++) {
+                System.out.println("图像1中的第" + (i + 1) + "张人脸的3D角度：（俯仰角）" + face3DAngleList1.get(i).getPitch() + ",（横滚角）" +
+                        face3DAngleList1.get(i).getRoll() + ",（偏航角）" + face3DAngleList1.get(i).getYaw()+System.lineSeparator());
+            }
+        }
+
+        /* 图像1中的人脸RGB活体值 */
+        {
+            // 创建图像1中的RGB活体信息列表
+            List<LivenessInfo> livenessInfoList1 = new ArrayList<>();
+            // 获取图像1中的RGB活体信息列表
+            FaceRecognitionUtils.getLiveness(livenessInfoList1);
+            // 将图像1中的RGB活体信息列表打印到控制台
+            for (int i = 0; i < livenessInfoList1.size(); i++) {
+                System.out.println("图像1中的第" + (i + 1) + "张人脸的RGB活体值：（未知=-1 、非活体=0 、活体=1、超出人脸=-2）" + livenessInfoList1.get(i).getLiveness()+System.lineSeparator());
+            }
+        }
+
+        /* 图像1中的人脸IR活体值 */
+        {
+            // 图像1进行IR活体检测（传入分离的图像信息数据）
+            FaceRecognitionUtils.detectionLiveness_IR1(file1);
+            // 创建图像1中的IR活体信息列表
+            List<IrLivenessInfo> irLivenessInfos1 = new ArrayList<>();
+            // 获取图像1中的IR活体信息列表
+            FaceRecognitionUtils.getIrLiveness(irLivenessInfos1);
+            // 将图像1中的IR活体信息列表打印到控制台
+            for (int i = 0; i < irLivenessInfos1.size(); i++) {
+                System.out.println("图像1中的第" + (i + 1) + "张人脸的IR活体值：（未知=-1 、非活体=0 、活体=1、超出人脸=-2）" + irLivenessInfos1.get(i).getLiveness()+System.lineSeparator());
+            }
+        }
+
+
+        System.out.println("--*--------------------------------【 图像2 】--------------------------------*--"+System.lineSeparator());
+
+
+        // 创建图像2中的人脸信息对象列表
+        List<FaceInfo> faceInfoList2=new ArrayList<>();
+        // 检测图像2中的人脸
+        FaceRecognitionUtils.faceDetection2(imageInfo2,faceInfoList2);
+        // 检测图像2中人脸属性
+        FaceRecognitionUtils.faceAttributeDetection2(imageInfo2,faceInfoList2);
+
+        /* 图像2中的人脸年龄 */
+        {
+            // 创建图像2中的人脸年龄列表
+            List<AgeInfo> ageInfoList2 = new ArrayList<>();
+            // 检测图像2中的人脸年龄列表
+            FaceRecognitionUtils.getAgeInfo(ageInfoList2);
+            // 将图像2中的年龄列表打印到控制台
+            for (int i = 0; i < ageInfoList2.size(); i++) {
+                System.out.println("图像2中的第" + (i + 1) + "张人脸的性别为：" + ageInfoList2.get(i).getAge()+System.lineSeparator());
+            }
+        }
+
+        /* 图像2中的人脸性别 */
+        {
+            // 创建图像2中的人脸性别列表
+            List<GenderInfo> genderInfoList2 = new ArrayList<>();
+            // 检测图像2中的人脸性别列表
+            FaceRecognitionUtils.getGender(genderInfoList2);
+            // 将图像2中的性别列表打印到控制台
+            for (int i = 0; i < genderInfoList2.size(); i++) {
+                String sex = genderInfoList2.get(i).getGender() == 0 ? "男" : "女";
+                System.out.println("图像2中的第" + (i + 1) + "张人脸的性别为：" + sex+System.lineSeparator());
+            }
+        }
+
+        /* 图像2中的人脸三维角度 */
+        {
+            // 创建图像2中的人脸三维角度信息列表
+            List<Face3DAngle> face3DAngleList2 = new ArrayList<>();
+            // 获取图像2中的人脸三维角度信息列表
+            FaceRecognitionUtils.getFace3DAngle(face3DAngleList2);
+            // 将图像2中的人脸三维角度信息列表打印到控制台
+            for (int i = 0; i < face3DAngleList2.size(); i++) {
+                System.out.println("图像2中的第" + (i + 1) + "张人脸的3D角度：（俯仰角）" + face3DAngleList2.get(i).getPitch() + ",（横滚角）" +
+                        face3DAngleList2.get(i).getRoll() + ",（偏航角）" + face3DAngleList2.get(i).getYaw()+System.lineSeparator());
+            }
+        }
+
+        /* 图像2中的人脸RGB活体值 */
+        {
+            // 创建图像2中的RGB活体信息列表
+            List<LivenessInfo> livenessInfoList2 = new ArrayList<>();
+            // 获取图像2中的RGB活体信息列表
+            FaceRecognitionUtils.getLiveness(livenessInfoList2);
+            // 将图像2中的RGB活体信息列表打印到控制台
+            for (int i = 0; i < livenessInfoList2.size(); i++) {
+                System.out.println("图像2中的第" + (i + 1) + "张人脸的RGB活体值：（未知=-1 、非活体=0 、活体=1、超出人脸=-2）" + livenessInfoList2.get(i).getLiveness()+System.lineSeparator());
+            }
+        }
+
+        /* 图像2中的人脸IR活体值 */
+        {
+            // 图像1进行IR活体检测（传入分离的图像信息数据）
+            FaceRecognitionUtils.detectionLiveness_IR1(file2);
+            // 创建图像2中的IR活体信息列表
+            List<IrLivenessInfo> irLivenessInfos2 = new ArrayList<>();
+            // 获取图像2中的IR活体信息列表
+            FaceRecognitionUtils.getIrLiveness(irLivenessInfos2);
+            // 将图像2中的IR活体信息列表打印到控制台
+            for (int i = 0; i < irLivenessInfos2.size(); i++) {
+                System.out.println("图像2中的第" + (i + 1) + "张人脸的IR活体值：（未知=-1 、非活体=0 、活体=1、超出人脸=-2）" + irLivenessInfos2.get(i).getLiveness()+System.lineSeparator());
+            }
+        }
+
+
+        System.out.println("--*--------------------------【 图像1 】与【图像2】人脸比对--------------------------*--"+System.lineSeparator());
+
+
+        /* 比对两张图像中的人脸特征 */
+        {
+            for (int i = 0; i < faceInfoList1.size(); i++) {
+                for (int i1 = 0; i1 < faceInfoList2.size(); i1++) {
+                    // 提取图像1中的第n张人脸特征
+                    FaceFeature faceFeature1 = FaceRecognitionUtils.faceFeatureExtraction1(imageInfo1, faceInfoList1.get(i));
+                    // 提取图像2中的第n张人脸特征
+                    FaceFeature faceFeature2 = FaceRecognitionUtils.faceFeatureExtraction1(imageInfo2, faceInfoList2.get(i1));
+                    // 人脸特征比对,得到相似度
+                    Float similarity = FaceRecognitionUtils.faceFeatureComparison(faceFeature1, faceFeature2);
+                    /**
+                     * 人脸比对取值范围为[0~1]，推荐阈值如下，高于此阈值的即可判断为
+                     * 用于生活照之间的特征比对，推荐阈值0.80
+                     * 用于证件照或生活照与证件照之间的特征比对，推荐阈值0.82
+                     */
+                    if (similarity > 0.8) {
+                        BigDecimal bd = BigDecimal.valueOf(similarity * 100).setScale(0, BigDecimal.ROUND_DOWN);
+                        System.out.println("图像1中的第" + (i + 1) + "张人脸与图像2中第" + (i1 + 1) + "张人脸匹配成功,相似度高达" + bd + "%！！！"+System.lineSeparator());
+                    }
+                }
+            }
+        }
+
+
+        //销毁SDK引擎
+        FaceRecognitionUtils.destroyTheSDKEngine();
+
+    }
+}
---
+++ src/com/company/utils/FaceRecognitionUtils.java
@@ -0,0 +1,501 @@
+package com.company.utils;
+
+import com.arcsoft.face.*;
+import com.arcsoft.face.enums.*;
+import com.arcsoft.face.toolkit.ImageInfo;
+import com.arcsoft.face.toolkit.ImageInfoEx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.arcsoft.face.toolkit.ImageFactory.getGrayData;
+
+/**
+ * @ClassName : FaceRecognitionUtils
+ * @Description : 人脸识别工具类
+ * @Author :
+ * @Date: 2020-04-14 11:35
+ */
+public class FaceRecognitionUtils{
+
+    // APP ID，请先在虹软开发者中心注册、认证之后创建应用获取
+    private static final String APP_ID="应用的APP ID";
+    // SDK KEY，请先在虹软开发者中心注册、认证之后创建应用获取
+    private static final String SDK_KEY="应用的APP ID";
+
+
+    private final static Logger logger= LoggerFactory.getLogger(FaceRecognitionUtils.class);
+
+    //人脸引擎
+    private static FaceEngine faceEngine=new FaceEngine();
+
+    //创建引擎功能对象(用于初始化引擎)
+    private static FunctionConfiguration functionConfiguration1 = new FunctionConfiguration();
+
+    //创建引擎功能对象(用于人脸检测)
+    private static FunctionConfiguration functionConfiguration2 = new FunctionConfiguration();
+
+    static{
+        // 初始化引擎功能（用于初始化引擎）
+        {
+            // 是否支持年龄检测功能
+            functionConfiguration1.setSupportAge(true);
+            // 是否支持3D检测功能
+            functionConfiguration1.setSupportFace3dAngle(true);
+            // 是否支持人脸检测功能
+            functionConfiguration1.setSupportFaceDetect(true);
+            // 是否支持人脸识别功能
+            functionConfiguration1.setSupportFaceRecognition(true);
+            // 是否支持性别检测功能
+            functionConfiguration1.setSupportGender(true);
+            // 是否支持RGB活体检测功能
+            functionConfiguration1.setSupportLiveness(true);
+            // 是否支持IR活体检测功能
+            functionConfiguration1.setSupportIRLiveness(true);
+        }
+        // 初始化引擎功能（用于人脸检测）
+        {
+            // 是否支持年龄检测功能
+            functionConfiguration2.setSupportAge(true);
+            // 是否支持3D检测功能
+            functionConfiguration2.setSupportFace3dAngle(true);
+            // 是否支持性别检测功能
+            functionConfiguration2.setSupportGender(true);
+            // 是否支持RGB活体检测功能
+            functionConfiguration2.setSupportLiveness(true);
+        }
+    }
+
+    /**
+     * 在线激活SDK
+     * @return
+     */
+    public static void sdkActivation(){
+        int errorCode=faceEngine.activeOnline(APP_ID,SDK_KEY);
+        if(errorCode!= ErrorInfo.MOK.getValue() && errorCode!=ErrorInfo.MERR_ASF_ALREADY_ACTIVATED.getValue()){
+            // SDK激活失败
+            logger.info("在线激活SDK失败！错误码："+errorCode);
+        }else{
+            // SDK激活成功
+            logger.info("在线激活SDK成功！");
+        }
+
+    }
+
+
+    /**
+     * 初始化引擎
+     * @param detectMode 检测模式（推荐IMAGE 模式）
+     * @param detectOrient 人脸检测角度（推荐人脸检测角度，逆时针0度）
+     * @param faceMaxNum 人脸检测最大数量（推荐10）
+     * @param faceScaleVal 最小人脸比例（VIDEO模式推荐16；IMAGE模式推荐32）
+     */
+    public static void InitializeTheEngine(DetectMode detectMode,DetectOrient detectOrient,int faceMaxNum,int faceScaleVal){
+        // 创建引擎配置类
+        EngineConfiguration engineConfiguration=new EngineConfiguration();
+        // 设置detectMode参数
+        engineConfiguration.setDetectMode(detectMode);
+        // 设置detectFaceOrientPriority参数
+        engineConfiguration.setDetectFaceOrientPriority(detectOrient);
+        // 设置人脸检测最大数量
+        engineConfiguration.setDetectFaceMaxNum(faceMaxNum);
+        // 设置detectFaceScaleVal参数为：识别的最小人脸比例 = 图片长边 / 人脸框长边的比值
+        engineConfiguration.setDetectFaceScaleVal(faceScaleVal);
+        // 配置引擎功能
+        engineConfiguration.setFunctionConfiguration(functionConfiguration1);
+
+        // 初始化引擎
+        int errorCode=faceEngine.init(engineConfiguration);
+        if(errorCode != ErrorInfo.MOK.getValue()){
+            // 引擎初始化失败
+            logger.info("引擎初始化失败！错误码："+errorCode);
+        }else{
+            // 引擎初始化成功
+            logger.info("引擎初始化成功！");
+        }
+    }
+
+
+    /**
+     * 人脸检测（传入分离的图像信息数据）
+     * @param imageInfo 图像信息
+     * @param faceInfoList 人脸信息列表
+     * @return 检测结果，检测成功或是失败！
+     */
+    public static boolean faceDetection1(ImageInfo imageInfo,List<FaceInfo> faceInfoList){
+        int errorCode = faceEngine.detectFaces(imageInfo.getImageData(),
+                imageInfo.getWidth(), imageInfo.getHeight(), imageInfo.getImageFormat(),
+                faceInfoList);
+        if(errorCode != ErrorInfo.MOK.getValue()){
+            // 人脸检测失败
+            logger.info("人脸检测失败！错误码："+errorCode);
+            return false;
+        }else{
+            // 人脸检测成功
+            logger.info("人脸检测成功！");
+            return true;
+        }
+    }
+
+
+    /**
+     * 人脸检测（传入ImageInfoEx图像信息数据）
+     * @param imageInfo 图像信息
+     * @param faceInfoList 人脸信息列表
+     * @return 检测结果，检测成功或是失败！
+     */
+    public static boolean faceDetection2(ImageInfo imageInfo,List<FaceInfo> faceInfoList){
+        ImageInfoEx imageInfoEx = new ImageInfoEx();
+        imageInfoEx.setHeight(imageInfo.getHeight());
+        imageInfoEx.setWidth(imageInfo.getWidth());
+        imageInfoEx.setImageFormat(imageInfo.getImageFormat());
+        imageInfoEx.setImageDataPlanes(new byte[][]{imageInfo.getImageData()});
+        imageInfoEx.setImageStrides(new int[]{imageInfo.getWidth() * 3});
+        int errorCode = faceEngine.detectFaces(imageInfoEx,
+                DetectModel.ASF_DETECT_MODEL_RGB, faceInfoList);
+        if(errorCode != ErrorInfo.MOK.getValue()){
+            // 人脸检测失败
+            logger.info("人脸检测失败！错误码："+errorCode);
+            return false;
+        }else{
+            // 人脸检测成功
+            logger.info("人脸检测成功！");
+            return true;
+        }
+    }
+
+
+    /**
+     * 人脸特征提取(传入分离的图像信息数据)
+     * @param imageInfo 图像信息
+     * @param faceInfo 人脸信息
+     * @return  人脸特征
+     */
+    public static FaceFeature faceFeatureExtraction1(ImageInfo imageInfo,FaceInfo faceInfo){
+        FaceFeature faceFeature = new FaceFeature();
+        int errorCode = faceEngine.extractFaceFeature(imageInfo.getImageData(),
+                imageInfo.getWidth(), imageInfo.getHeight(), imageInfo.getImageFormat(),
+                faceInfo, faceFeature);
+        if(errorCode != ErrorInfo.MOK.getValue()){
+            // 人脸特征提取失败
+            logger.info("人脸特征提取失败！错误码："+errorCode);
+            return null;
+        }else{
+            // 人脸特征提取成功
+            logger.info("人脸特征提取成功！");
+            return faceFeature;
+        }
+    }
+
+
+    /**
+     * 人脸特征提取(传入ImageInfoEx图像信息数据)
+     * @param imageInfo 图像信息
+     * @param faceInfo 人脸信息
+     * @return  人脸特征
+     */
+    public static FaceFeature faceFeatureExtraction2(ImageInfo imageInfo,FaceInfo faceInfo){
+        ImageInfoEx imageInfoEx = new ImageInfoEx();
+        imageInfoEx.setHeight(imageInfo.getHeight());
+        imageInfoEx.setWidth(imageInfo.getWidth());
+        imageInfoEx.setImageFormat(imageInfo.getImageFormat());
+        imageInfoEx.setImageDataPlanes(new byte[][]{imageInfo.getImageData()});
+        imageInfoEx.setImageStrides(new int[]{imageInfo.getWidth() * 3});
+
+        // 创建人脸特征对象
+        FaceFeature faceFeature = new FaceFeature();
+        int errorCode =  faceEngine.extractFaceFeature(imageInfoEx, faceInfo, faceFeature);
+        if(errorCode != ErrorInfo.MOK.getValue()){
+            // 人脸特征提取失败
+            logger.info("人脸特征提取失败！错误码："+errorCode);
+            return null;
+        }else{
+            // 人脸特征提取成功
+            logger.info("人脸特征提取成功！");
+            return faceFeature;
+        }
+    }
+
+
+    /**
+     * 人脸特征比对
+     * @param targetFaceFeature 目标人脸特征
+     * @param sourceFaceFeature 来源人脸特征
+     * @param compareModel 比对模型
+     * @return 比对相似度
+     */
+    public static Float faceFeatureComparison(FaceFeature targetFaceFeature, FaceFeature sourceFaceFeature,CompareModel compareModel){
+        // 创建比对相似度对象
+        FaceSimilar faceSimilar=new FaceSimilar();
+        int errorCode=faceEngine.compareFaceFeature(targetFaceFeature,sourceFaceFeature, compareModel,faceSimilar);
+        if(errorCode != ErrorInfo.MOK.getValue()){
+            // 人脸特征比对失败
+            logger.info("人脸特征比对失败！错误码："+errorCode);
+            return null;
+        }else{
+            // 人脸特征比对成功
+            logger.info("人脸特征比对成功！");
+            return faceSimilar.getScore();
+        }
+    }
+
+
+    /**
+     * 人脸特征比对（默认LIFE_PHOTO比对模型）
+     * @param targetFaceFeature 目标人脸特征
+     * @param sourceFaceFeature 来源人脸特征
+     * @return 比对相似度
+     */
+    public static Float faceFeatureComparison(FaceFeature targetFaceFeature, FaceFeature sourceFaceFeature){
+        // 创建比对相似度对象
+        FaceSimilar faceSimilar=new FaceSimilar();
+        int errorCode = faceEngine.compareFaceFeature(targetFaceFeature, sourceFaceFeature,
+                faceSimilar);
+        if(errorCode != ErrorInfo.MOK.getValue()){
+            // 人脸特征比对失败
+            logger.info("人脸特征比对失败！错误码："+errorCode);
+            return null;
+        }else{
+            // 人脸特征比对成功
+            logger.info("人脸特征比对成功！");
+            return faceSimilar.getScore();
+        }
+    }
+
+
+    /**
+     * 人脸属性检测（传入分离的图像信息数据）
+     * @param imageInfo 图像信息
+     * @param faceInfoList 人脸信息列表
+     * @return 检测结果，检测成功或是失败！
+     */
+    public static boolean faceAttributeDetection1(ImageInfo imageInfo,List<FaceInfo> faceInfoList){
+        int errorCode = faceEngine.process(imageInfo.getImageData(), imageInfo.getWidth(),
+                imageInfo.getHeight(), imageInfo.getImageFormat(), faceInfoList, functionConfiguration2);
+        if(errorCode != ErrorInfo.MOK.getValue()){
+            // 人脸属性检测失败
+            logger.info("人脸属性检测失败！错误码："+errorCode);
+            return false;
+        }else{
+            // 人脸属性检测成功
+            logger.info("人脸属性检测成功！");
+            return true;
+        }
+    }
+
+
+    /**
+     * 人脸属性检测（传入ImageInfoEx图像信息数据）
+     * @param imageInfo 图像信息
+     * @param faceInfoList 人脸信息列表
+     * @return 检测结果，检测成功或是失败！
+     */
+    public static boolean faceAttributeDetection2(ImageInfo imageInfo,List<FaceInfo> faceInfoList){
+        ImageInfoEx imageInfoEx = new ImageInfoEx();
+        imageInfoEx.setHeight(imageInfo.getHeight());
+        imageInfoEx.setWidth(imageInfo.getWidth());
+        imageInfoEx.setImageFormat(imageInfo.getImageFormat());
+        imageInfoEx.setImageDataPlanes(new byte[][]{imageInfo.getImageData()});
+        imageInfoEx.setImageStrides(new int[]{imageInfo.getWidth() * 3});
+        int errorCode = faceEngine.process(imageInfoEx, faceInfoList,
+                functionConfiguration2);
+        if(errorCode != ErrorInfo.MOK.getValue()){
+            // 人脸属性检测失败
+            logger.info("人脸属性检测失败！错误码："+errorCode);
+            return false;
+        }else{
+            // 人脸属性检测成功
+            logger.info("人脸属性检测成功！");
+            return true;
+        }
+    }
+
+
+    /**
+     * 获取年龄信息
+     * 注意：人脸属性检测之后方可调用
+     * @return 获取结果，获取失败或是成功!
+     */
+    public static boolean getAgeInfo(List<AgeInfo> ageInfoList){
+        int errorCode = faceEngine.getAge(ageInfoList);
+        if(errorCode!=ErrorInfo.MOK.getValue()){
+            // 获取年龄信息失败
+            logger.info("获取年龄信息失败！错误码："+errorCode);
+            return false;
+        }else{
+            // 已成功获取年龄信息
+            logger.info("已成功获取年龄信息！");
+            return true;
+        }
+    }
+
+
+    /**
+     * 获取性别（0为男性，1为女性。）
+     * 注意：人脸属性检测之后方可调用
+     * @return 获取结果，获取失败或是成功!
+     */
+    public static boolean getGender(List<GenderInfo> genderInfoList){
+        // 性别检测
+        int errorCode = faceEngine.getGender(genderInfoList);
+        if(errorCode!=ErrorInfo.MOK.getValue()){
+            // 获取性别失败
+            logger.info("获取性别失败！错误码："+errorCode);
+            return false;
+        }else{
+            // 已成功获取年龄信息
+            logger.info("已成功获取性别！");
+            return true;
+        }
+    }
+
+
+    /**
+     * 获取人脸三维角度信息
+     * 人脸3D角度：俯仰角（pitch）， 横滚角（roll）， 偏航角（yaw）。
+     * 注意：人脸属性检测之后方可调用
+     * @return 获取结果，获取失败或是成功!
+     */
+    public static boolean getFace3DAngle(List<Face3DAngle> face3DAngleList){
+        // 人脸三维角度检测
+        int errorCode = faceEngine.getFace3DAngle(face3DAngleList);
+        if(errorCode!=ErrorInfo.MOK.getValue()){
+            // 获取人脸三维角度信息失败
+            logger.info("获取人脸三维角度信息失败！错误码："+errorCode);
+            return false;
+        }else{
+            // 已成功获取人脸三维角度信息
+            logger.info("已成功获取人脸三维角度信息！");
+            return true;
+        }
+    }
+
+
+    /**
+     * 获取RGB活体信息
+     * RGB活体值，未知=-1 、非活体=0 、活体=1、超出人脸=-2
+     * 注意：人脸属性检测之后方可调用
+     * @return 获取结果，获取失败或是成功!
+     */
+    public static boolean getLiveness(List<LivenessInfo> livenessInfoList){
+        // RGB活体检测
+        int errorCode = faceEngine.getLiveness(livenessInfoList);
+        if(errorCode!=ErrorInfo.MOK.getValue()){
+            // 获取RGB活体信息失败
+            logger.info("获取RGB活体信息失败！错误码："+errorCode);
+            return false;
+        }else{
+            // 已成功获取RGB活体信息
+            logger.info("已成功获取RGB活体信息！");
+            return true;
+        }
+    }
+
+
+    /**
+     * IR活体检测（传入分离的图像信息数据）
+     * 注意：
+     *     引擎需要支持IR活体检测功能
+     * @return 检测结果，检测成功或是失败！
+     */
+    public static boolean detectionLiveness_IR1(File file){
+        // 创建图像信息
+        ImageInfo imageInfoGray = getGrayData(file);
+        // 创建人脸信息列表
+        List<FaceInfo> faceInfoListGray = new ArrayList<FaceInfo>();
+        // 人脸检测（传入分离的图像信息数据）
+        int errorCode1 = faceEngine.detectFaces(imageInfoGray.getImageData(),
+                imageInfoGray.getWidth(), imageInfoGray.getHeight(),
+                imageInfoGray.getImageFormat(), faceInfoListGray);
+        // 创建引擎功能实例对象
+        FunctionConfiguration configuration = new FunctionConfiguration();
+        // 设置引擎支持IR活体检测
+        configuration.setSupportIRLiveness(true);
+        // IR活体检测
+        int errorCode2 = faceEngine.processIr(imageInfoGray.getImageData(),
+                imageInfoGray.getWidth(), imageInfoGray.getHeight(),
+                imageInfoGray.getImageFormat(), faceInfoListGray, configuration);
+        if(errorCode1!=ErrorInfo.MOK.getValue() || errorCode2!=ErrorInfo.MOK.getValue()){
+            String errorCode=errorCode1==0?errorCode2+"":errorCode1+"";
+            // IR活体检测失败
+            logger.info("IR活体检测失败！错误码："+errorCode);
+            return false;
+        }else{
+            // IR活体检测成功
+            logger.info("IR活体检测成功！");
+            return true;
+        }
+    }
+
+
+    /**
+     * IR活体检测（传入ImageInfoEx图像信息数据）
+     * 注意：
+     *     引擎需要支持年龄检测功能
+     * @param imageInfo 图像信息
+     * @return 检测结果，检测成功或是失败！
+     */
+    public static boolean detectionLiveness_IR2(ImageInfo imageInfo){
+        ImageInfoEx imageInfoEx = new ImageInfoEx();
+        imageInfoEx.setHeight(imageInfo.getHeight());
+        imageInfoEx.setWidth(imageInfo.getWidth());
+        imageInfoEx.setImageFormat(imageInfo.getImageFormat());
+        imageInfoEx.setImageDataPlanes(new byte[][]{imageInfo.getImageData()});
+        imageInfoEx.setImageStrides(new int[]{imageInfo.getWidth() * 3});
+        List<FaceInfo> faceInfoList1 = new ArrayList<>();
+        int errorCode1 = faceEngine.detectFaces(imageInfoEx,
+                DetectModel.ASF_DETECT_MODEL_RGB, faceInfoList1);
+        FunctionConfiguration fun = new FunctionConfiguration();
+        fun.setSupportAge(true);
+        int errorCode2 = faceEngine.processIr(imageInfoEx, faceInfoList1,
+                fun);
+        if(errorCode1!=ErrorInfo.MOK.getValue() || errorCode2!=ErrorInfo.MOK.getValue()){
+            String errorCode=errorCode1==0?errorCode2+"":errorCode1+"";
+            // IR活体检测失败
+            logger.info("IR活体检测失败！错误码："+errorCode);
+            return false;
+        }else{
+            // IR活体检测成功
+            logger.info("IR活体检测成功！");
+            return true;
+        }
+    }
+
+
+    /**
+     * 获取IR活体信息
+     * IR活体值，未知=-1 、非活体=0 、活体=1、超出人脸=-2
+     * @return 获取结果，获取失败或是成功!
+     */
+    public static boolean getIrLiveness(List<IrLivenessInfo> irLivenessInfo){
+        // IR活体检测
+        int errorCode = faceEngine.getLivenessIr(irLivenessInfo);
+        if(errorCode!=ErrorInfo.MOK.getValue()){
+            // 获取IR活体信息失败
+            logger.info("获取IR活体信息失败！错误码："+errorCode);
+            return false;
+        }else{
+            // 已成功获取IR活体信息
+            logger.info("已成功获取IR活体信息！");
+            return true;
+        }
+    }
+
+
+    /**
+     * 销毁SDK引擎
+     */
+    public static void destroyTheSDKEngine(){
+        int errorCode = faceEngine.unInit();
+        if(errorCode!=ErrorInfo.MOK.getValue()){
+            // 销毁SDK引擎失败
+            logger.info("销毁SDK引擎失败！错误码："+errorCode);
+        }else{
+            // 销毁SDK引擎成功
+            logger.info("销毁SDK引擎成功！");
+        }
+    }
+
+}
